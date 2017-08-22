8dd90f81d31db7ef62d2a4b0c5a8445b19dae7b8
Ian David Garcia <cinquedia@gmail.com>
2017-08-22 18:41:33 +0800

    Editor

---
+++ Source/Code/FellSky.Editor/FellSkyEditorPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Editor
+{
+    public class FellSkyEditorPlugin : Duality.Editor.EditorPlugin
+    {
+        public override string Id => "FellSkyEditorPlugin";
+    }
+}
